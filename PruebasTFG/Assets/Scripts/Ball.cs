dbd43cd6a45d65fd868121968ba004c5ba84d3b4
DanielAlvarezCastro <dalvar06@ucm.es>
2021-03-29 13:14:35 +0200

    New walking Anim and ball launcher

---
+++ PruebasTFG/Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float lifeTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnEnable()
+    {
+        Invoke("SetInactive", lifeTime);
+    }
+
+    void SetInactive() { gameObject.SetActive(false); }
+
+}
